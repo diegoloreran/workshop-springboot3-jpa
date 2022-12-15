139867a33521d0e62679727614dbc2b521a72af4
Diego Loreran <diegoloreran@gmail.com>
2022-12-15 14:30:43 -0300

    Project created - JPA repository, dependency injection, database seeding

---
+++ src/main/java/diegoteste/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package diegoteste.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import diegoteste.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
