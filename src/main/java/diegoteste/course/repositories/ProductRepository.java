b92d4bd4602a83600b907ba84eed928bde0874ca
Diego Loreran <diegoloreran@gmail.com>
2022-12-24 11:11:16 -0300

    Product

---
+++ src/main/java/diegoteste/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package diegoteste.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import diegoteste.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
---
+++ src/main/java/diegoteste/course/services/ProductService.java
@@ -0,0 +1,27 @@
+package diegoteste.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import diegoteste.course.entities.Product;
+import diegoteste.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
