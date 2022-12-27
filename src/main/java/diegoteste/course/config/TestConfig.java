3017a5b57b8535d7b3c223f2958062066a2d74a8
Diego Loreran <diegoloreran@gmail.com>
2022-12-27 17:35:45 -0300

    Payment, one-to-one association

--- src/main/java/diegoteste/course/config/TestConfig.java
+++ src/main/java/diegoteste/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import diegoteste.course.entities.Category;
 import diegoteste.course.entities.Order;
 import diegoteste.course.entities.OrderItem;
+import diegoteste.course.entities.Payment;
 import diegoteste.course.entities.Product;
 import diegoteste.course.entities.User;
 import diegoteste.course.entities.enums.OrderStatus;
@@ -80,5 +81,9 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 }
